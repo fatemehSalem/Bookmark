ac0db1ab779d4b2ba58fb0bf31e8cfc76d045752
Fatemeh <42536170+fatemehSalem@users.noreply.github.com>
2023-08-25 15:35:23 +0330

    some new changes

---
+++ src/test/java/com/demo/bookmarker/api/BookmarkControllerTest.java
@@ -0,0 +1,15 @@
+package com.demo.bookmarker.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class BookmarkControllerTest {
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    private MockMvc mvc;
+
+}
